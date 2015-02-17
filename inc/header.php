c654b916d2e8d28423c7ec3c0133bdb7c896d8a3
Linda Gibson <dakidz@live.com>
2015-02-16 21:42:36 -0800

    create a header for website

---
+++ inc/header.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+  <title></title>
+  <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+
+  <div class="header">
+
+    <div class="wrapper">
+
+      <h1 class="branding-title"><a href="./">Luv2Stay</a></h1>
+
+      <ul class="nav">
+        <li class="rentals">Rentals</li>
+        <li class="contact">Contact</li>
+      </ul>
+
+    </div>
+
+  </div>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,1 +1,5 @@
+<?php
+include("inc/header.php");
+?>
 
+  <h1>This is to test this page</h1>
