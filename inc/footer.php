d8d9942915e2a77e8ca49955f3af91f61b66614b
Linda Gibson <dakidz@live.com>
2015-02-16 22:35:06 -0800

    create a footer page and include it on index page

---
+++ inc/footer.php
@@ -0,0 +1,9 @@
+<div class="footer">
+
+  <div class="wrapper">
+
+    <p>&copy;<?php echo date('Y'); ?> Luv2Stay </p>
+
+  </div>
+
+</div>
--- index.php
+++ index.php
@@ -3,3 +3,12 @@ include("inc/header.php");
 ?>
 
   <h1>This is to test this page</h1>
+
+
+
+
+
+
+<?php
+include("inc/footer.php");
+?>
