01a7ea2eb55289b5c4b0c4a0807fffe6541a9a56
Linda Gibson <dakidz@live.com>
2015-02-22 17:58:22 -0800

    create listings to include on rentals page

---
+++ inc/listings.php
@@ -0,0 +1,44 @@
+<?php
+
+$listings = array();
+  $listings[101] = array(
+    "location" => "Jackson Hole",
+    "name" => "Comfy Cabin",
+    "bedrooms" => 2,
+    "baths" => 2,
+    "sleeps" => 6,
+    "img" => "img/comfycabin.jpeg",
+    "price" => 175
+  );
+
+  $listings[102] = array(
+    "location" => "San Diego",
+    "name" => "Cool Cabana",
+    "bedrooms" => 3,
+    "baths" => 2,
+    "sleeps" => 10,
+    "img" => "img/coolcabana.jpeg",
+    "price" => 300
+  );
+
+  $listings[103] = array(
+    "location" => "Long Beach",
+    "name" => "Surf's Up",
+    "bedrooms" => 2,
+    "baths" => 2,
+    "sleeps" => 5,
+    "img" => "img/surfsup.jpeg",
+    "price" => 250
+  );
+
+  $listings[104] = array(
+    "location" => "Waikiki",
+    "name" => "Peaceful Paradise",
+    "bedrooms" => 4,
+    "baths" => 2,
+    "sleeps" => 12,
+    "img" => "img/peacefulparadise.jpeg",
+    "price" => 500
+  );
+
+?>
