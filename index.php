2b26265123ea40ce6904aee57b14e3009aaa3bff
Linda Gibson <dakidz@live.com>
2015-02-22 17:08:21 -0800

    embed video clip onto index page as a banner

--- index.php
+++ index.php
@@ -1,8 +1,20 @@
 <?php
+$pageTitle="Places You Love To Stay";
 include("inc/header.php");
 ?>
 
-  <h1>This is to test this page</h1>
+  <div class="Section-Banner">
+
+    <div class="wrapper">
+
+      <embed src="img/boat.mp4.mp4" width="100%" height="300" controller="false" autoplay="true", muted="true", loop="true" class="mainvideo">
+
+      <h1>This is to test this page</h1>
+
+    </div>
+
+  </div>
+
 
 
 
