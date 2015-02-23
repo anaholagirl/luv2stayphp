ee5bf8e1099e3a5a2eb6031e788f1cc1740d6417
Linda Gibson <dakidz@live.com>
2015-02-22 17:27:36 -0800

    create a rentals.php page to list all rentals

---
+++ rentals.php
@@ -0,0 +1,17 @@
+
+<?php
+$pageTitle = "All Rental Listings";
+include("inc/header.php");
+?>
+
+  <div class="RentalList">
+
+    <div class="wrapper">
+
+      <h1>Complete Rental List</h1>
+
+    </div>
+
+  </div>
+
+<?php include("inc/footer.php"); ?>
