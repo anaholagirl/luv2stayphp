2869531a843b5dcc366b49188e39bdaa8cdf267f
Linda Gibson <dakidz@live.com>
2015-02-23 09:48:00 -0800

    add feature rental to index page

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 $pageTitle="Places You Love To Stay";
 include("inc/header.php");
 ?>
+<body>
+<div class="container">
 
   <div class="Section-Banner">
 
@@ -9,17 +11,43 @@ include("inc/header.php");
 
       <embed src="img/boat.mp4.mp4" width="100%" height="300" controller="false" autoplay="true", muted="true", loop="true" class="mainvideo">
 
-      <h1>This is to test this page</h1>
+      <h1>Welcome to Luv2Stay</h1>
+      <h2>Your Home Away From Home</h2>
 
     </div>
 
   </div>
 
+  <div class="featured-rental">
 
+    <div class="wrapper">
 
+      <h2>Feature Rental: </h2>
 
+      <?php include("inc/listings.php"); ?>
 
+        <ul class="listings">
+          <?php
 
+            $total_listings = count($listings);
+            $position = 0;
+            $list_view_html = "";
+            foreach($listings as $listing_id => $listing) {
+              $position = $position + 1;
+              if ($total_listings - $position < 1) {
+                $list_view_html = list_rentals_html($listing_id, $listing) . $list_view_html;
+              }
+            }
+            echo $list_view_html;
+          ?>
+        </ul>
+
+    </div>
+
+  </div>
+
+</div>
+</body>
 
 <?php
 include("inc/footer.php");
